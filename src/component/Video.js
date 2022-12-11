007d910d567d172fef42170076836b72f6937b50
ChuanTT <Chuan123789@gmail.com>
2022-12-11 19:38:14 +0700

    upload

---
+++ src/component/Video.js
@@ -0,0 +1,5 @@
+const Video = ({id, refVideo}) => {
+    return ( <video style={{backgroundColor: '#333', margin: '0 10px'}} height={200} width={200} id={id} ref={refVideo}></video>);
+}
+ 
+export default Video;
